b9583b67402c335835aab40c6f25a549c1e7b6a7
Levi Rucka <lruckaemail@gmail.com>
2025-01-25 23:35:01 -0800

    Create p1.c
    Project 1 for C programming language. Learned from CSE121 at Clark College.

---
+++ p1.c
@@ -0,0 +1,17 @@
+// Code for project 1, creates an easy print statement
+#include <stdio.h>
+
+int main (void)
+{
+  int a, b, c;
+
+  a = 10;
+  b = 20;
+
+  c = a + b;
+
+  printf("%i + %i = %i\n", a, b, c);
+  printf("That was easy!\n");
+
+  return 0;
+}
